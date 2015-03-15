3102ed495316c66013aafbfc7038311f89c74d3c
mbjd <balduindettling@gmail.com>
2015-03-15 17:22:50 +0100

    Button reversed & tabs are purple too

--- app/src/main/java/com/example/philipp/scrum/ProjectOverviewFragment.java
+++ app/src/main/java/com/example/philipp/scrum/ProjectOverviewFragment.java
@@ -32,41 +32,6 @@ public class ProjectOverviewFragment extends Fragment {
         String description = currentProject.getDescription();
         descriptionText.setText(description);
 
-        /*
-        // Button to change description and its onClickListener
-        Button changeDescButton = (Button) layout.findViewById(R.id.update_desc_button);
-
-        changeDescButton.setOnClickListener(new View.OnClickListener() {
-
-            /**
-             * This method handles clicks on the "update description" button. On a click, it pulls
-             * the text entered in R.id.project_description and sets it as the new description.
-             * @param v - the View that has been clicked. In our case this is always the same Button.
-             *./
-
-            @Override
-            public void onClick(View v) {
-
-                if (v.getId() == R.id.update_desc_button)
-                {
-                    // Get the EditText from the layout and the project index from the arguments
-                    EditText descEditText = (EditText) layout.findViewById(R.id.project_description);
-                    int projectPosition = getArguments().getInt("project");
-
-                    // Load the current Everything object
-                    Everything everything = new Everything();
-                    everything.load(getActivity().getApplicationContext());
-
-                    // Get the current project, set its description to the entered text and save it
-                    Project currentProject = everything.getProject(projectPosition);
-                    currentProject.setDescription(descEditText.getText().toString());
-                    everything.setProject(projectPosition, currentProject);
-                }
-
-            }
-        });
-        */
-
         return layout;
     }
 
